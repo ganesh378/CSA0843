bd56759a7c2596189ddc30e7bf2f79f2166848c0
ganesh378 <112242608+ganesh378@users.noreply.github.com>
2022-09-30 14:43:59 +0530

    Add files via upload

---
+++ Vowels in string.py
@@ -0,0 +1,7 @@
+A=input("enter a string")
+vowels=0
+for i in A:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+        vowels=vowels+1
+        print("the vowels are",vowels)
+
